e577c3e2e5048df2931942fb4fd518e47dcf02e5
shariful8523 <mdsharifulislam8523@gmail.com>
2025-04-15 23:04:47 +0600

    added the last news section and navbar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 
@@ -6,7 +7,14 @@ import Navbar from "../components/Navbar";
 const HomeLayout = () => {
     return (
         <div>
-            <Header></Header>
+
+            <section>
+               <Header></Header>  
+               <section className="w-11/12 mx-auto justify-center mt-8">
+                <LatestNews></LatestNews>
+               </section>
+            </section>
+           
             <Navbar></Navbar>
             <Main></Main>
             
--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Marquee from "react-fast-marquee";
+
+
 
 const LatestNews = () => {
     return (
-        <div>
-            
+        <div className='flex  bg-[#F3F3F3] '>
+            <div className='flex ml-2 py-4 gap-5 items-center'>
+                <p className='bg-[#D72050]  text-base-100 px-3 py-2 ml-5 font-bold ' >Latest</p>
+                <Marquee>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur culpa ab rem itaque natus ducimus in assumenda est molestiae dolore libero eos a esse unde, maiores laborum incidunt! Voluptates, sed!</p>
+
+                </Marquee>
+
+            </div>
+
+
         </div>
     );
 };
